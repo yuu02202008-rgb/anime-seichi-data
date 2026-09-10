e2804911acf6757054c273acc2fc682cde5e5a4b
yuu02202008-rgb <yuu.0220.2008@icloud.com>
2026-09-10 19:05:34 +0900

    Add files via upload

--- official-sources.js
+++ official-sources.js
@@ -1,6 +1,10 @@
 // 公式サイトであることを確認できた作品だけを登録します。
 // 画像の利用許可が得られた場合は、ここに artworkUrl を追加して表示できます。
 window.officialSources = {
+  "ATRI -My Dear Moments-": {
+    artworkUrl: "https://artworks.thetvdb.com/banners/v4/series/425244/posters/6669aaea129c9.jpg",
+    artworkCredit: "Artwork archive"
+  },
   "ご注文はうさぎですか？": {
     siteUrl: "https://gochiusa.com/01/",
     siteLabel: "TVアニメ『ご注文はうさぎですか？』公式サイト"
